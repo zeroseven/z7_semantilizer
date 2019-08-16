16dc3c88ae06fc329385b27a92ee2362b4144ea8
Raphael Thanner <r.thanner@zeroseven.de>
2019-08-16 11:40:48 +0200

    Enable or disable the validation. Store the users decision in the backend user authentification session

--- Classes/Hooks/DrawHeaderHook.php
+++ Classes/Hooks/DrawHeaderHook.php
@@ -3,15 +3,13 @@
 namespace Zeroseven\Semantilizer\Hooks;
 
 use TYPO3\CMS\Backend\Routing\UriBuilder;
-use TYPO3\CMS\Backend\Template\Components\Buttons\LinkButton;
-use TYPO3\CMS\Core\Imaging\Icon;
+use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
 use TYPO3\CMS\Core\Imaging\IconFactory;
 use TYPO3\CMS\Core\Messaging\FlashMessage;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Fluid\View\StandaloneView;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Core\Database\ConnectionPool;
-
 
 class DrawHeaderHook
 {
@@ -28,11 +26,20 @@ class DrawHeaderHook
     /** @var array */
     protected $contentElements = [];
 
+    /**  @var BackendUserAuthentication */
+    protected $backendUser;
+
+    /** @var bool */
+    protected $validationEnabled = true;
+
+    /** @var string */
+    private const validationSessionKey = 'semantilizer_validation';
+
     /** @var string */
     private const table = 'tt_content';
 
     /** @var array */
-    private const state = [
+    private const states = [
         'notice' => FlashMessage::NOTICE,
         'info' => FlashMessage::INFO,
         'ok' => FlashMessage::OK,
@@ -45,7 +52,33 @@ class DrawHeaderHook
         $this->uriBuilder = GeneralUtility::makeInstance(UriBuilder::class);
         $this->pageInfo = BackendUtility::readPageAccess((int)GeneralUtility::_GP('id'), true);
         $this->iconFactory = GeneralUtility::makeInstance(IconFactory::class);
+        $this->backendUser = GeneralUtility::makeInstance(BackendUserAuthentication::class);
         $this->contentElements = $this->collectContentElements();
+        $this->validationEnabled = $this->setValidationCookie();
+    }
+
+    private function setValidationCookie(): bool
+    {
+        $validate = GeneralUtility::_GP(self::validationSessionKey);
+
+        if($validate === null) {
+            $sessionData = $this->backendUser->getSessionData(self::validationSessionKey, 1);
+            return $sessionData === null ? $this->validationEnabled : (bool)$sessionData;
+        }
+
+        $this->backendUser->setSessionData(self::validationSessionKey, (int)$validate);
+
+        return (bool)$validate;
+
+    }
+
+    private function getToggleValidationLink(): string
+    {
+        return $this->uriBuilder->buildUriFromRoute('web_layout', [
+            self::validationSessionKey => (int)!$this->validationEnabled,
+            'id' => $this->pageInfo['uid'],
+            'returnUrl' => GeneralUtility::getIndpEnv('REQUEST_URI')
+        ]);
     }
 
     private function getEditRecordUrl(int $uid, string $table = null): string
@@ -63,47 +96,28 @@ class DrawHeaderHook
     private function getMoveHierarchyLink(array $row, int $move): ?string
     {
         $currentType = (int)$row['header_type'];
+        $newType = $move > 0 ? $currentType - 1 : $currentType + 1;
         $lowestType = 1;
         $highestType = $lowestType;
 
+        // Get the highest type from the tca configuration
         foreach ($GLOBALS['TCA'][self::table]['columns']['header_type']['config']['items'] as $item) {
             $highestType = (int)max($highestType, $item[1]);
         }
 
-        if($move > 0) {
-            $newType = $currentType - 1;
-            if($newType < $lowestType) {
-                return null;
-            }
-        } else {
-            $newType = $currentType + 1;
-            if($newType > $highestType) {
-                return null;
-            }
+        // Check, if the new type is valid
+        if($newType < $lowestType || $newType > $highestType) {
+            return null;
         }
 
         return BackendUtility::getLinkToDataHandlerAction(
             sprintf('&data[%s][%d][%s]=%d', self::table, $row['uid'], 'header_type', $newType),
             GeneralUtility::getIndpEnv('REQUEST_URI')
         );
-
-
-//        return GeneralUtility::makeInstance(LinkButton::class)
-//            // TODO: ->setTitle(htmlspecialchars($this->getLanguageService()->sL('LLL:EXT:z7_responsive_carousel/Resources/Private/Language/locallang_be.xlf:responsivecarousel.controls.hide')))
-//            ->setClasses($moveIcon . ' ' . $disabledClass)
-//            ->setHref(BackendUtility::getLinkToDataHandlerAction(
-//                sprintf('&data[%s][%d][%s]=%d', self::table, $row['uid'], 'header_type', $newType),
-//                GeneralUtility::getIndpEnv('REQUEST_URI')
-//            ))
-//            ->setIcon($this->iconFactory->getIcon($moveIcon, Icon::SIZE_SMALL))
-//            ->render();
     }
 
     protected function collectContentElements(): array
     {
-
-        // The returning array
-        $contentElements = [];
 
         // Get instance of the query builder
         $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable(self::table);
@@ -121,22 +135,13 @@ class DrawHeaderHook
             ->fetchAll() ?: [];
 
         // Convert the keys to lowerCamelCase
-//        foreach ($results as $index => $row) {
-//            foreach ($row as $key => $value) {
-//                $contentElements[$index][GeneralUtility::underscoredToLowerCamelCase($key)] = $value;
-//            }
-//        }
-//
-//        return $contentElements;
-    }
-
-    /**
-     * Returns the language service
-     * @return LanguageService
-     */
-    protected function getLanguageService()
-    {
-        return $GLOBALS['LANG'];
+        //foreach ($results as $index => $row) {
+        //    foreach ($row as $key => $value) {
+        //        $contentElements[$index][GeneralUtility::underscoredToLowerCamelCase($key)] = $value;
+        //    }
+        //}
+        //
+        //return $contentElements;
     }
 
     /**
@@ -156,8 +161,10 @@ class DrawHeaderHook
         $view = GeneralUtility::makeInstance(StandaloneView::class);
         $view->setTemplatePathAndFilename(GeneralUtility::getFileAbsFileName('EXT:z7_semantilizer/Resources/Private/Templates/Backend/PageHeader.html'));
         $view->assignMultiple([
-            'state' => self::state,
-            'contentElements' => $this->contentElements
+            'state' => self::states['notice'],
+            'contentElements' => $this->contentElements,
+            'validationEnabled' => $this->validationEnabled,
+            'toggleValidationLink' => $this->getToggleValidationLink()
         ]);
 
         return $view->render();
