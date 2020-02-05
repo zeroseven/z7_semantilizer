ae37ead2670fdfe1b80cbb4f2c807a373e9ec709
Raphael Thanner <r.thanner@zeroseven.de>
2020-02-05 15:25:20 +0100

    Display the header overview in selected language

--- Classes/Hooks/DrawHeaderHook.php
+++ Classes/Hooks/DrawHeaderHook.php
@@ -4,6 +4,8 @@ namespace Zeroseven\Semantilizer\Hooks;
 
 use TYPO3\CMS\Backend\Routing\UriBuilder;
 use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
+use TYPO3\CMS\Core\Database\Connection;
+use TYPO3\CMS\Core\Database\Query\Restriction\HiddenRestriction;
 use TYPO3\CMS\Core\Imaging\IconFactory;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Fluid\View\StandaloneView;
@@ -62,7 +64,31 @@ class DrawHeaderHook
     private function simulateFrontend(): TypoScriptFrontendController
     {
 
-        $TSFE = GeneralUtility::makeInstance(TypoScriptFrontendController::class, $GLOBALS['TYPO3_CONF_VARS'], (int)GeneralUtility::_GP('id'), (int)GeneralUtility::_GP('type'));
+        $pageUid = (int)GeneralUtility::_GP('id');
+
+        // Get the translated page uid
+        if($this->modulData['language']) {
+            $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('pages');
+
+            // Remove 'AND (hidden = 0)' from the query
+            $queryBuilder->getRestrictions()->removeByType(HiddenRestriction::class);
+
+            // Create query
+            $pageUid = (int)$queryBuilder
+                ->select('uid')
+                ->from('pages')
+                ->where($queryBuilder->expr()->andX(
+                    $queryBuilder->expr()->eq('l10n_parent', $queryBuilder->createNamedParameter($pageUid, \PDO::PARAM_INT)),
+                    $queryBuilder->expr()->in('sys_language_uid', $queryBuilder->createNamedParameter($this->modulData['language'], Connection::PARAM_INT))
+                ))
+                ->setMaxResults(1)
+                ->execute()
+                ->fetchColumn(0) ?: $pageUid;
+        }
+
+        // Hi Kasper Skaarhoj, if you read this:
+        // I'm not sure anymore. This is really the best way to render typoscript by this hook?
+        $TSFE = GeneralUtility::makeInstance(TypoScriptFrontendController::class, $GLOBALS['TYPO3_CONF_VARS'], $pageUid, (int)GeneralUtility::_GP('type'));
         $TSFE->set_no_cache();
         $TSFE->initFEuser();
         $TSFE->fetch_the_id();
