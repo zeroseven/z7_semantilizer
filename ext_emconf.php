8186935a35c616664d71d3db3ce49bc47b497a9d
Napora <napora@drblitz-weblab.com>
2020-05-15 10:00:17 +0200

    Update typo3 depends

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.4.99',
+            'typo3' => '10.4.2-10.9.99',
         ],
         'conflicts' => [
         ],
