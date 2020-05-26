0d468de1fcafb7e3e1f0052390ff158f1db3033a
Raphael Thanner <r.thanner@zeroseven.de>
2020-05-26 23:37:00 +0200

    Get content elements by utility class

--- Classes/Hooks/DrawHeaderHook.php
+++ Classes/Hooks/DrawHeaderHook.php
@@ -6,13 +6,11 @@ use TYPO3\CMS\Backend\Routing\UriBuilder;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Fluid\View\StandaloneView;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
-use TYPO3\CMS\Core\Database\ConnectionPool;
-use Zeroseven\Semantilizer\FixedTitle\FixedTitleInterface;
 use Zeroseven\Semantilizer\Models\ContentCollection;
 use Zeroseven\Semantilizer\Services\BootstrapColorService;
 use Zeroseven\Semantilizer\Services\HideNotificationStateService;
 use Zeroseven\Semantilizer\Services\TsConfigService;
-use Zeroseven\Semantilizer\Models\ContentData;
+use Zeroseven\Semantilizer\Utilities\CollectContentUtility;
 use Zeroseven\Semantilizer\Models\PageData;
 use Zeroseven\Semantilizer\Utilities\ValidationUtility;
 
@@ -37,15 +35,11 @@ class DrawHeaderHook
     /** @var string */
     private const VALIDATION_PARAMETER = 'semantilizer_hide_notifications';
 
-    /** @var string */
-    private const TABLE = 'tt_content';
-
     public function __construct()
     {
         $this->page = PageData::makeInstance();
         $this->tsConfig = TsConfigService::getTsConfig($this->page->getUid());
         $this->moduleData = BackendUtility::getModuleData([], null, 'web_layout');
-        $this->contentCollection = GeneralUtility::makeInstance(ContentCollection::class);
         $this->hideNotifications = $this->setValidationCookie();
     }
 
@@ -67,67 +61,6 @@ class DrawHeaderHook
             self::VALIDATION_PARAMETER => (int)!$this->hideNotifications,
             'id' => $this->page->getUid(),
         ]);
-    }
-
-    private function collectContentElements(): void
-    {
-
-        // Get instance of the query builder
-        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable(self::TABLE);
-
-        // Get results from the database
-        $results = $queryBuilder->select('uid', 'header', 'header_type', 'cType')
-            ->from(self::TABLE)
-            ->where(
-                $queryBuilder->expr()->gt('header_type', $queryBuilder->createNamedParameter(0, \PDO::PARAM_INT)),
-                $queryBuilder->expr()->lt('header_layout', $queryBuilder->createNamedParameter(100, \PDO::PARAM_INT)),
-                $queryBuilder->expr()->neq('header', $queryBuilder->createNamedParameter('')),
-                $queryBuilder->expr()->eq('pid', $queryBuilder->createNamedParameter($this->page->getUid(), \PDO::PARAM_INT)),
-                $queryBuilder->expr()->eq('sys_language_uid', $queryBuilder->createNamedParameter((int)$this->moduleData['language'], \PDO::PARAM_INT)),
-                $queryBuilder->expr()->notIn('CType', array_map(function ($value) use ($queryBuilder) {
-                    return $queryBuilder->createNamedParameter($value, \PDO::PARAM_STR);
-                }, (array)GeneralUtility::trimExplode(',', $this->tsConfig['ignoreCTypes'])))
-            )
-            ->orderBy('sorting')
-            ->execute()
-            ->fetchAll() ?: [];
-
-        // Add some links and attributes to the content elements
-        foreach ($results as $row) {
-            $contentElement = GeneralUtility::makeInstance(ContentData::class, $row);
-            $this->contentCollection->append($contentElement);
-        }
-    }
-
-    private function setFixedTitle(): void
-    {
-        $fixedTitle = null;
-        $hookParameter = [
-            'uid' => $this->page->getUid(),
-            'sys_language_uid' => (int)$this->moduleData['language'],
-            'page' => $this->page,
-            'tsConfig' => $this->tsConfig
-        ];
-
-        if ($hooks = $GLOBALS['TYPO3_CONF_VARS']['EXT']['z7_semantilizer']['fixedPageTitle'] ?? null) {
-
-            // Sort them by their keys
-            ksort($hooks);
-
-            // Loop them
-            foreach ($hooks as $className) {
-                if (empty($fixedTitle) && class_exists($className) && is_subclass_of($className, FixedTitleInterface::class)) {
-                    if ($overrideTitle = GeneralUtility::callUserFunction($className . '->get', $hookParameter, $this)) {
-                        $fixedTitle = $overrideTitle;
-                    }
-                }
-            }
-        }
-
-        if ($fixedTitle) {
-            $contentElement = GeneralUtility::makeInstance(ContentData::class, ['header' => $fixedTitle, 'headerType' => 1, '__fixed' => true]);
-            $this->contentCollection->prepend($contentElement);
-        }
     }
 
     private function skipSemantilizer(): bool
@@ -155,10 +88,8 @@ class DrawHeaderHook
         }
 
         // Collect the content elements
-        $this->collectContentElements();
-
-        // Add prepend title
-        $this->setFixedTitle();
+        $collectContentUtility = GeneralUtility::makeInstance(CollectContentUtility::class, $this->page->getUid(), (int)$this->moduleData['language'], $this->tsConfig, $this->page);
+        $this->contentCollection = $collectContentUtility->getCollection();
 
         // Validate
         $validationUtility = GeneralUtility::makeInstance(ValidationUtility::class, $this->contentCollection);
---
+++ Classes/Utilities/CollectContentUtility.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace Zeroseven\Semantilizer\Utilities;
+
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use Zeroseven\Semantilizer\FixedTitle\FixedTitleInterface;
+use Zeroseven\Semantilizer\Models\ContentCollection;
+use Zeroseven\Semantilizer\Models\ContentData;
+use Zeroseven\Semantilizer\Models\PageData;
+
+class CollectContentUtility
+{
+
+    /** @var string */
+    private const TABLE = 'tt_content';
+
+    /** @var int */
+    private $pid;
+
+    /** @var int */
+    private $language;
+
+    /** @var array */
+    private $tsConfig;
+
+    /** @var PageData */
+    private $page;
+
+    public function __construct(int $pid, int $language, array $tsConfig, PageData $page = null)
+    {
+        $this->pid = $pid;
+        $this->language = $language;
+        $this->tsConfig = $tsConfig;
+        $this->page = $page;
+    }
+
+    protected function getFixedTitle(): ?ContentData
+    {
+        $fixedTitle = null;
+        $hookParameter = [
+            'uid' => $this->page->getUid(),
+            'sys_language_uid' => $this->language,
+            'page' => $this->page,
+            'tsConfig' => $this->tsConfig
+        ];
+
+        if ($hooks = $GLOBALS['TYPO3_CONF_VARS']['EXT']['z7_semantilizer']['fixedPageTitle'] ?? null) {
+
+            // Sort them by their keys
+            ksort($hooks);
+
+            // Loop them
+            foreach ($hooks as $className) {
+                if (empty($fixedTitle) && class_exists($className) && is_subclass_of($className, FixedTitleInterface::class)) {
+                    if ($overrideTitle = GeneralUtility::callUserFunction($className . '->get', $hookParameter, $this)) {
+                        $fixedTitle = $overrideTitle;
+                    }
+                }
+            }
+        }
+
+        if ($fixedTitle) {
+            return GeneralUtility::makeInstance(ContentData::class, ['header' => $fixedTitle, 'headerType' => 1, '__fixed' => true]);
+        }
+
+        return null;
+    }
+
+    public function getCollection(): ContentCollection
+    {
+
+        // Create collection
+        $contentCollection = GeneralUtility::makeInstance(ContentCollection::class);
+
+        // Get instance of the query builder
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable(self::TABLE);
+
+        // Get results from the database
+        $results = $queryBuilder->select('uid', 'header', 'header_type', 'cType')
+            ->from(self::TABLE)
+            ->where(
+                $queryBuilder->expr()->gt('header_type', $queryBuilder->createNamedParameter(0, \PDO::PARAM_INT)),
+                $queryBuilder->expr()->lt('header_layout', $queryBuilder->createNamedParameter(100, \PDO::PARAM_INT)),
+                $queryBuilder->expr()->neq('header', $queryBuilder->createNamedParameter('')),
+                $queryBuilder->expr()->eq('pid', $queryBuilder->createNamedParameter($this->pid, \PDO::PARAM_INT)),
+                $queryBuilder->expr()->eq('sys_language_uid', $queryBuilder->createNamedParameter($this->language, \PDO::PARAM_INT)),
+                $queryBuilder->expr()->notIn('CType', array_map(static function ($value) use ($queryBuilder) {
+                    return $queryBuilder->createNamedParameter($value, \PDO::PARAM_STR);
+                }, $this->tsConfig['ignoreCTypes'] ? GeneralUtility::trimExplode(',', $this->tsConfig['ignoreCTypes']) : []))
+            )
+            ->orderBy('sorting')
+            ->execute()
+            ->fetchAll() ?: [];
+
+        // Add some links and attributes to the content elements
+        foreach ($results as $row) {
+            $contentElement = GeneralUtility::makeInstance(ContentData::class, $row);
+            $contentCollection->append($contentElement);
+        }
+
+        // Prepend Title
+        if($fixedTitle = $this->getFixedTitle()) {
+            $contentCollection->prepend($fixedTitle);
+        }
+
+        return $contentCollection;
+    }
+
+}
