0ec4923a4c2c4675575bb74845fc8a9c6b8e1391
Raphael Thanner <r.thanner@zeroseven.de>
2020-05-26 23:51:11 +0200

    Collect faulty headings in widget controller

--- Classes/Widgets/FaultyHeadingsWidget.php
+++ Classes/Widgets/FaultyHeadingsWidget.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Zeroseven\Semantilizer\Widgets;
 
 use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Messaging\FlashMessage;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Dashboard\Widgets\ButtonProviderInterface;
 use TYPO3\CMS\Dashboard\Widgets\ChartDataProviderInterface;
@@ -11,10 +12,10 @@ use TYPO3\CMS\Dashboard\Widgets\ListDataProviderInterface;
 use TYPO3\CMS\Dashboard\Widgets\WidgetConfigurationInterface;
 use TYPO3\CMS\Dashboard\Widgets\WidgetInterface;
 use TYPO3\CMS\Fluid\View\StandaloneView;
-use Zeroseven\Semantilizer\FixedTitle\FixedTitleInterface;
-use Zeroseven\Semantilizer\Services\PageInfoService;
+use Zeroseven\Semantilizer\Services\BootstrapColorService;
+use Zeroseven\Semantilizer\Utilities\CollectContentUtility;
 use Zeroseven\Semantilizer\Services\TsConfigService;
-use Zeroseven\Semantilizer\Utilities\PageData;
+use Zeroseven\Semantilizer\Models\PageData;
 use Zeroseven\Semantilizer\Utilities\ValidationUtility;
 
 class FaultyHeadingsWidget implements WidgetInterface
@@ -44,13 +45,54 @@ class FaultyHeadingsWidget implements WidgetInterface
         $this->dataProvider = $dataProvider;
     }
 
+    protected function validatePage(int $pageId): ?array
+    {
+        // Load the tsConfig
+        $tsConfig = TsConfigService::getTsConfig($pageId);
+
+        // Semantilizer is disabled on given page
+        if (($disableOnPages = $tsConfig['disableOnPages']) && in_array($pageId, GeneralUtility::intExplode(',', $disableOnPages), true)) {
+            return null;
+        }
+
+        // Page object
+        $page = PageData::makeInstance($pageId);
+
+        // Skip some doktypes
+        if ($page->isIgnoredDoktype()) {
+            return null;
+        }
+
+        // Get content elements
+        $collectContentUtility = GeneralUtility::makeInstance(CollectContentUtility::class, $pageId, 0, $tsConfig, $page);
+        $contentCollection = $collectContentUtility->getCollection();
+
+        // Validate
+        $validationUtility = GeneralUtility::makeInstance(ValidationUtility::class, $contentCollection);
+
+        // Set error state
+        foreach ($validationUtility->getAffectedContentElements() as $affected) {
+            $this->contentCollection->overrideElement($affected);
+        }
+
+        if(($status = $validationUtility->getStrongestLevel()) > FlashMessage::OK) {
+            return [
+                'page' => $page,
+                'status' => $status,
+                'color' => BootstrapColorService::getColorByFlashMessageState($status)
+            ];
+        }
+
+        return null;
+    }
+
     protected function getPages(): array
     {
         // Get instance of the query builder
         $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('tt_content');
 
         // Get results from the database
-        $results = $queryBuilder->select('pid', 'header', 'header_type', 'cType')
+        $pageUids = $queryBuilder->select('pid')
             ->from('tt_content')
             ->where(
                 $queryBuilder->expr()->gt('header_type', $queryBuilder->createNamedParameter(0, \PDO::PARAM_INT)),
@@ -58,68 +100,21 @@ class FaultyHeadingsWidget implements WidgetInterface
                 $queryBuilder->expr()->neq('header', $queryBuilder->createNamedParameter('')),
                 $queryBuilder->expr()->eq('sys_language_uid', $queryBuilder->createNamedParameter(0, \PDO::PARAM_INT))
             )
-            ->orderBy('pid')
+            ->orderBy('tstamp')
             ->addOrderBy('sorting')
+            ->groupBy('pid')
             ->execute()
             ->fetchAll() ?: [];
 
-        // Group content elements into pages
+        // Create pages
         $pages = [];
-        foreach ($results as $i => $row) {
-            foreach ($row as $key => $value) {
-                $pages[$row['pid']][$i][GeneralUtility::underscoredToLowerCamelCase($key)] = $value;
-            }
-        }
+        foreach ($pageUids as $row) {
 
-        // Fixed title hook
-        if ($fixedPageTitleHooks = $GLOBALS['TYPO3_CONF_VARS']['EXT']['z7_semantilizer']['fixedPageTitle'] ?? null) {
-            ksort($fixedPageTitleHooks);
-        }
+            // Get the uid of the page
+            $uid = (int)$row['pid'];
 
-        // Clean up pages by tsConfig and prepend fix titles
-        foreach ($pages as $uid => $contentElements) {
-
-            // Get tsConfig of each page
-            $tsConfig = TsConfigService::getTsConfig((int)$uid);
-
-            // Remove disabled pages
-            if (($disableOnPages = $tsConfig['disableOnPages']) && in_array((int)$uid, GeneralUtility::intExplode(',', $disableOnPages), true)) {
-                unset($pages[$uid]);
-            }
-
-            // Remove ignored CTypes
-            if ($ignoreCTypes = GeneralUtility::trimExplode(',', $tsConfig['ignoreCTypes'] ?? null)) {
-                foreach ($contentElements as $i => $contentElement) {
-                    if (in_array($contentElement['ctype'], $ignoreCTypes, true)) {
-                        unset($pages[$uid][$i]);
-                    }
-                }
-            }
-
-            // Remove empty pages
-            if (count($contentElements) === 0) {
-                unset($pages[$uid]);
-            }
-
-            // Set fixed Title
-            if ($fixedPageTitleHooks) {
-                $fixedTitle = null;
-
-                foreach ($fixedPageTitleHooks as $className) {
-                    if (empty($fixedTitle) && class_exists($className) && is_subclass_of($className, FixedTitleInterface::class)) {
-
-                        $params = [
-                            'uid' => (int)$uid,
-                            'sys_language_uid' => 0,
-                            'row' => PageInfoService::getPageInfo((int)$uid),
-                            'tsConfig' => $tsConfig
-                        ];
-
-                        if ($fixedTitle = GeneralUtility::callUserFunction($className . '->get', $params)) {
-                            $pages[$uid] = [['header' => $fixedTitle, 'headerType' => 1]] + $pages[$uid];
-                        }
-                    }
-                }
+            if($page = $this->validatePage($uid)) {
+                $pages[] = $page;
             }
         }
 
@@ -129,16 +124,9 @@ class FaultyHeadingsWidget implements WidgetInterface
     public function renderWidgetContent(): string
     {
 
-        var_dump(PageData::makeInstance(1));
         $this->view->setTemplatePathAndFilename('EXT:z7_semantilizer/Resources/Private/Templates/Widget/FaultyHeadings.html');
 
-        foreach ($this->getPages() ?? [] as $uid => $contentElements) {
-            if ($status = GeneralUtility::makeInstance(ValidationUtility::class, $contentElements)->getStrongestLevel()) {
-                if ($status > 0) {
-                    var_dump($uid);
-                }
-            }
-        }
+        var_dump($this->getPages());
 
         $this->view->assignMultiple([
             'options' => $this->options,
