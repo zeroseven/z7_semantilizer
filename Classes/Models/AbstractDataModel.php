a48b69f09a18a83206e8e8ccf7605f8e87decf62
Raphael Thanner <r.thanner@zeroseven.de>
2020-05-30 13:54:39 +0200

    Add getter to the abstract data model to access all data in fixedTitle hooks

--- Classes/Models/AbstractDataModel.php
+++ Classes/Models/AbstractDataModel.php
@@ -21,4 +21,30 @@ abstract class AbstractDataModel
 
         $this->data = $data;
     }
+
+    public function getData(string $property): ?string
+    {
+        if(isset($this->data[$property])) {
+            return trim((string)$this->data[$property]);
+        }
+
+        return null;
+    }
+
+    public function getInt(string $property): int
+    {
+        $value = $this->getData($property);
+
+        // Check if the value is an integer
+        if (is_numeric($value)) {
+            return (int)$value;
+        }
+
+        throw new \Exception(sprintf('Property "%s" can not be converted to a number', $property));
+    }
+
+    public function toArray(): array
+    {
+        return $this->data;
+    }
 }
