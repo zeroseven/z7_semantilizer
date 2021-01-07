2522a72f417eba3ec22c849bd3d3b4bda4c18625
Sebastian Rosskopf <s.rosskopf@zeroseven.de>
2021-01-07 10:17:36 +0000

    Update composer.json to be installable under TYPO311

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,10 +11,10 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
-    'version' => '2.1.1',
+    'version' => '2.2.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.4.99',
+            'typo3' => '9.5.0-11.0.99',
         ],
         'conflicts' => [
         ],
