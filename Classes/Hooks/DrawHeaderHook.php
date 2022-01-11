9daeceef66a3b6063cc2fd26732b4c53b47a7c47
Raphael Thanner <r.thanner@zeroseven.de>
2022-01-11 21:54:18 +0100

    Add semantilizer class into global TYPO3 namespace

--- Classes/Hooks/DrawHeaderHook.php
+++ Classes/Hooks/DrawHeaderHook.php
@@ -93,13 +93,14 @@ class DrawHeaderHook
         $id = GeneralUtility::quoteJSvalue($this->identifier);
         $contentSelectors = json_encode(GeneralUtility::trimExplode(',', $this->tsConfig['contentSelectors']));
 
+        // Configure page renderer
         $this->pageRenderer->loadRequireJsModule('TYPO3/CMS/Z7Semantilizer/Backend/Semantilizer');
         $this->pageRenderer->addCssFile('EXT:z7_semantilizer/Resources/Public/Css/Backend/Styles.css');
         $this->pageRenderer->addInlineLanguageLabelFile('EXT:z7_semantilizer/Resources/Private/Language/locallang.xlf', 'overview');
         $this->pageRenderer->addInlineLanguageLabelFile('EXT:z7_semantilizer/Resources/Private/Language/locallang.xlf', 'notification');
         $this->pageRenderer->addJsFooterInlineCode(self::class, sprintf('
             require(["TYPO3/CMS/Z7Semantilizer/Backend/Semantilizer"], function(Semantilizer) {
-                Semantilizer = new Semantilizer(%s, %s, %s);
+                TYPO3.Semantilizer = top.TYPO3.Semantilizer = new Semantilizer(%s, %s, %s);
             });
         ', $url, $id, $contentSelectors));
     }
