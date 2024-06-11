ca2f56ace3d9dbabc49da492a34ff6e8fd54d9a4
Raphael Thanner <r.thanner@zeroseven.de>
2024-06-11 12:23:15 +0200

    Update list of ignored doktypes

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '4.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-12.4.99'
+            'typo3' => '12.4.0-13.1.99'
         ],
         'conflicts' => [
         ],
