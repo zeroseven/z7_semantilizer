c47fc92aebfa515982bf84c51256393fd7e08dda
Raphael Thanner <r.thanner@zeroseven.de>
2024-11-05 15:22:52 +0100

    Use new notification event listener in TYPO3 13

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '4.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-13.1.99'
+            'typo3' => '13.1.0-13.4.99'
         ],
         'conflicts' => [
         ],
