4518986717e825cee94bbe14f9f9949210f52def
Raphael Thanner <raphael.thanner@udg.de>
2025-08-01 11:07:27 +0200

    Remove ext_localconf.php to resolve merge conflict and clean up deprecated configuration

--- ext_localconf.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-defined('TYPO3') || die('✘');
-
-call_user_func(static function () {
-    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig("@import 'EXT:z7_semantilizer/Configuration/Sets/Semantilizer/page.tsconfig'");
-});
